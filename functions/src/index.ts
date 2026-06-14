6103e790ee544d2783b1a6ef2e320397d5820641
juan giacosa <juangiacosabrk@gmail.com>
2026-06-14 08:52:47 -0300

    Cloud Functions fijada a Gen 1 — imports explícitos a /v1 evitan upgrade forzado a Gen 2

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v1";
 import * as admin from "firebase-admin";
 import { telegramBot } from "./bot/telegram";
 import { log } from "./helpers/logger";
